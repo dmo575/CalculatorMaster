d04bc3e9be61d63bf811351cbd9da59d67dfa5fc
dmo575 <dmo5@protonmail.com>
2023-08-29 15:50:02 -0700

    Cleanup

--- static/js/index.js
+++ static/js/index.js
@@ -61,7 +61,6 @@ const pladeholderFlagImgSrc = '/static/images/world.png';
 // start point
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    document.documentElement.requestFullscreen();
     openMessagePre(introModal, true);
 });
 
